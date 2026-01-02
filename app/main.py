9dfa076da594529f44bc931d33b11c118ae17305
Marcos Vinicius <mviniciuslp87@alu.ufc.br>
2026-01-02 18:04:37 -0300

    feat: CORS

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.routers.user_router import router as user_router
 
 app = FastAPI(
@@ -6,6 +7,12 @@ app = FastAPI(
     version="0.1.0"
 )
 
-app = FastAPI(title="Users LIP API")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],     
+    allow_credentials=True,
+    allow_methods=["*"],     
+    allow_headers=["*"],     
+)
 
 app.include_router(user_router)
